c9a75dad35cf377e47757b14c9ee6cce952efb0d
D4N00 <daniel.viskup@gmail.com>
2026-04-08 15:47:02 +0200

    Add files via upload

---
+++ ultrasound.srcs/sources_1/new/HC_SR04_CTL.vhd
@@ -0,0 +1,115 @@
+----------------------------------------------------------------------------------
+
+-- Create Date: 08.04.2026 14:31:45
+-- Module Name: HC_SR04_CTL - Behavioral
+-- Project Name: Ultrasound 13
+-- Target Devices: Nexys A7-50T
+-- Description: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity HC_SR04_CTL is
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           start : in STD_LOGIC;
+           trig : out STD_LOGIC;
+           echo : in STD_LOGIC;
+           distance : out STD_LOGIC_VECTOR (16 downto 0));
+
+           
+end HC_SR04_CTL;
+
+architecture Behavioral of HC_SR04_CTL is
+    
+    constant CLK_FREQ : integer := 100_000_000;             -- System clock frequency (100 MHz)
+    constant trig_count : integer := CLK_FREQ/1000 * 15;    -- Enter the trigger time here (15us) 
+    
+    -- FSM state definitions
+    type state_type is (IDLE, SEND_TRIG, WAIT_ECHO, CNT_ECHO, CALC);
+    signal current_state : state_type;
+
+    signal clock_count : integer range 0 to 999_999_999;
+    
+    
+begin
+    p_measure : process (clk)
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                -- Reset state, outputs, and all internal signals
+                current_state     <= IDLE;             -- Start in IDLE state
+                trig              <= '0';              -- disable trigger
+                distance          <= (others => '0');  -- reset distance output
+                clock_count       <= 0;                -- reset the counter
+
+            else
+                case current_state is
+
+                    -- IDLE: Wait for the start signal to trigger measurement
+                    when IDLE =>
+                        trig <= '0'; -- disable trigger
+
+                        if start = '1' then
+                            trig <= '1';                -- trigger the sensor
+                            clock_count <= 0;           -- reset the counter
+                            current_state <= SEND_TRIG; -- move to SEND_TRIG state
+                        end if;
+
+                    -- SEND_TRIG: Trigger the sensor with a 10+us pulse
+                    when SEND_TRIG =>
+                          
+                        if clock_count  >=  trig_count then
+                            trig <= '0';                    -- end the trigger pulse
+                            clock_count   <= 0;             -- Reset clock count
+                            current_state <= WAIT_ECHO;     -- move to WAIT_ECHO state
+                        else
+                            clock_count <= clock_count + 1; --increment the clock counter
+                        end if;   
+                        
+                        
+                    -- WAIT_ECHO: wait until the start of the echo pulse
+                    when WAIT_ECHO =>
+
+                        if echo = '1' then
+                            clock_count   <= 0;         -- Reset clock count
+                            current_state <= CNT_ECHO;  -- move to CNT_ECHO state
+                        end if;
+                            
+                    -- CNT_ECHO: count clock cycle pulses in echo pulse
+                    when CNT_ECHO =>
+                        
+                        if echo = '1' then
+                            clock_count <= clock_count + 1; --increment the clock counter   
+                        else
+                            distance <= std_logic_vector(to_unsigned((clock_count/CLK_FREQ/1000), 16)); -- distance is for now equal to echo round trip time in us
+                            clock_count   <= 0;             -- Reset clock count
+                            current_state <= IDLE;     -- move to WAIT_ECHO state
+                        end if; 
+                        
+                    -- Default: In case of an unexpected state, return to IDLE
+                    when others =>
+                        current_state <= IDLE;
+
+                end case;
+            end if;
+        end if;
+    end process p_measure;
+
+end Behavioral;
---
+++ ultrasound.srcs/sources_1/new/tb_HC_SR04_CTL.vhd
@@ -0,0 +1,90 @@
+-- Testbench automatically generated online
+-- at https://vhdl.lapinoo.net
+-- Generation date : Wed, 08 Apr 2026 13:39:45 GMT
+-- Request id : cfwk-fed377c2-69d65aa1346df
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_HC_SR04_CTL is
+end tb_HC_SR04_CTL;
+
+architecture tb of tb_HC_SR04_CTL is
+
+    component HC_SR04_CTL
+        port (clk      : in std_logic;
+              rst      : in std_logic;
+              start    : in std_logic;
+              trig     : out std_logic;
+              echo     : in std_logic;
+              distance : out std_logic_vector (16 downto 0));
+    end component;
+
+    signal clk      : std_logic;
+    signal rst      : std_logic;
+    signal start    : std_logic;
+    signal trig     : std_logic;
+    signal echo     : std_logic;
+    signal distance : std_logic_vector (16 downto 0);
+
+    constant TbPeriod : time := 1000 ns; -- ***EDIT*** Put right period here
+    signal TbClock : std_logic := '0';
+    signal TbSimEnded : std_logic := '0';
+
+begin
+
+    dut : HC_SR04_CTL
+    port map (clk      => clk,
+              rst      => rst,
+              start    => start,
+              trig     => trig,
+              echo     => echo,
+              distance => distance);
+
+    -- Clock generation
+    TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
+
+    -- ***EDIT*** Check that clk is really your main clock signal
+    clk <= TbClock;
+
+    stimuli : process
+    begin
+        -- ***EDIT*** Adapt initialization as needed
+        start <= '0';
+        echo <= '0';
+
+        -- Reset generation
+        -- ***EDIT*** Check that rst is really your reset signal
+        rst <= '1';
+        wait for 100 ns;
+        rst <= '0';
+        wait for 100 ns;
+        
+        
+        start <= '1';
+        wait for 100 ns;
+        start <= '0';
+        
+        wait for 40us;
+        
+        echo <= '1';
+        wait for 250us;
+        echo <= '0';
+        
+            
+        -- ***EDIT*** Add stimuli here
+        wait for 100 * TbPeriod;
+
+        -- Stop the clock and hence terminate the simulation
+        TbSimEnded <= '1';
+        wait;
+    end process;
+
+end tb;
+
+-- Configuration block below is required by some simulators. Usually no need to edit.
+
+configuration cfg_tb_HC_SR04_CTL of tb_HC_SR04_CTL is
+    for tb
+    end for;
+end cfg_tb_HC_SR04_CTL;
